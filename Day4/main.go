e7b4cb1e700cf1f0c42cfa8b7c73781d8a96d986
Seaweedbeard <ruben.bjorkoy@gmail.com>
2024-12-05 18:46:18 +0100

    Finished task 1

--- Day4/main.go
+++ Day4/main.go
@@ -1,78 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	file, err := os.Open("input")
-
-	if err != nil {
-        panic(err)
-    }
-
-
-	defer file.Close()
-
-	reader := bufio.NewScanner(file)
-
-	// var direction = 0
-	crossWord := []string{}
-	solution := [][][]int{} //A three dimensional array that contains the index of 'x' and 'm' from a complete 'xmas' solution location
-	var currentLocation = [2]int{0, 0}
-	const searchTerm = "XMAS"
-
-	for reader.Scan() {
-		var line = reader.Text()
-		crossWord = append(crossWord, line)
-	}
-	
-	for i := range(crossWord) {
-		for j := range(crossWord[i]) {
-			letter := crossWord[i][j]
-			currentWord := [][2]int{}
-			if(letter == 'X') {
-				currentLocation = [2]int{j, i}
-				fmt.Println("Found the letter ", string(letter))
-				fmt.Println("at ", currentLocation[0], ",", currentLocation[1])
-				currentWord = append(currentWord, currentLocation)
-				var nextLetter [2]int
-				
-				for (len(currentWord) < len(searchTerm)) {
-					nextLetter = findLetter(crossWord, currentLocation, searchTerm[len(currentWord)])
-					if(nextLetter != [2]int{-1, -1}) {
-						currentWord = append(currentWord, nextLetter)
-					} else {
-						break
-					}
-				}
-			}
-			if len(currentWord) == len(searchTerm) {
-				convertedWord := make([][]int, len(currentWord))
-				for k, v := range currentWord {
-					convertedWord[k] = v[:]
-				}
-				solution = append(solution, convertedWord)
-			}
-		}
-	}
-
-	fmt.Println(len(solution))
+	Task1()
+	Task2()
 }
 
-func findLetter(crossWord []string, currentLocation [2]int, letter byte) [2]int {
-	for y := -1; y<=1; y++ {
-		if(currentLocation[1] + y < 0 || currentLocation[1] + y >= len(crossWord)) {continue}
-		for x := -1; x<=1; x++ {
-			if(currentLocation[0] + x < 0 || currentLocation[0] + x >= len(crossWord[currentLocation[1] + y])) {continue}
-			if crossWord[currentLocation[1] + y][currentLocation[0] + x] == letter {
-				fmt.Println("Found the letter ", string(letter))
-				fmt.Println("at ", currentLocation[0] + x, ",", currentLocation[1] + y)
-				return [2]int{currentLocation[0] + x, currentLocation[1] + y}
-			}
-		}
+func findLetter(crossWord []string, currentLocation [2]int, letter byte, direction [2]int) ([2]int, [2]int) {
+	newY := currentLocation[1] + direction[1]
+	newX := currentLocation[0] + direction[0] 
+	
+	if newY < 0 || newY >= len(crossWord) || newX < 0 || newX >= len(crossWord[newY]) {
+		return [2]int{-1, -1}, [2]int{-2, -2}
 	}
-	return [2]int{-1, -1}
+
+	if crossWord[newY][newX] == letter {
+		fmt.Printf("Found the letter '%c' at %d, %d\n", letter, newX, newY)
+		return [2]int{newX, newY}, direction
+	}
+
+	fmt.Printf("Failed to find letter '%c' from location %d, %d in direction (%d, %d)\n", letter, currentLocation[0], currentLocation[1], direction[0], direction[1])
+	return [2]int{-1, -1}, [2]int{-2, -2}
+}
+
+func getDirections() [][2]int {
+	return [][2]int{
+		{-1, -1}, {0, -1}, {1, -1},
+		{-1, 0},           {1, 0},
+		{-1, 1},  {0, 1},  {1, 1},
+	}
 }
---
+++ Day4/task1.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Task1() {
+	file, err := os.Open("input")
+
+	if err != nil {
+        panic(err)
+    }
+
+
+	defer file.Close()
+
+	reader := bufio.NewScanner(file)
+
+	crossWord := []string{}
+	solutions := [][][]int{}
+	const searchTerm = "XMAS"
+
+	for reader.Scan() {
+		var line = reader.Text()
+		crossWord = append(crossWord, line)
+	}
+	
+	for i := range(crossWord) {
+		for j := range(crossWord[i]) {
+			letter := crossWord[i][j]
+			if letter == 'X' {
+				for _, initialDirection := range getDirections() {
+					currentWord := [][2]int{}
+					currentLocation := [2]int{j, i}
+					currentWord = append(currentWord, currentLocation)
+
+					for len(currentWord) < len(searchTerm) {
+						nextLetter, newDirection := findLetter(crossWord, currentLocation, searchTerm[len(currentWord)], initialDirection)
+
+						if nextLetter != [2]int{-1, -1} {
+							currentWord = append(currentWord, nextLetter)
+							currentLocation = nextLetter
+							initialDirection = newDirection
+						} else {
+							break
+						}
+					}
+
+					if len(currentWord) == len(searchTerm) {
+						convertedWord := make([][]int, len(currentWord))
+						for k, v := range currentWord {
+							convertedWord[k] = v[:]
+						}
+						solutions = append(solutions, convertedWord)
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println("Total solutions found:", len(solutions))
+}
