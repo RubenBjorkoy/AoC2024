f51b6a06607eeff0afe7f4dcae3429f40ebe90db
Seaweedbeard <ruben.bjorkoy@gmail.com>
2024-12-06 16:27:15 +0100

    Solved AoC day 6 part 1

--- Day6/main.go
+++ Day6/main.go
@@ -8,28 +8,74 @@ import (
 )
 
 func main() {
-	file, err := os.Open("testInput")
+	file, err := os.Open("input")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
 	var fileArr [][]string
-	direction := 3
+	var currentPosition [2]int
+	var direction int
 
+	scanner := bufio.NewScanner(file)
+	row := 0
 	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		fileArr = append(fileArr, []string{line})
+		line := strings.Split(strings.TrimSpace(scanner.Text()), "")
+		fileArr = append(fileArr, line)
+		for col, char := range line {
+			if char == "^" || char == "v" || char == "<" || char == ">" {
+				currentPosition = [2]int{col, row}
+				switch char {
+				case "^":
+					direction = 0
+				case ">":
+					direction = 1
+				case "v":
+					direction = 2
+				case "<":
+					direction = 3
+				}
+				fileArr[row][col] = "."
+			}
+		}
+		row++
 	}
-	fmt.Println(direction)
-	direction = changeDirection(direction)
-	fmt.Println(direction)
 
-	fmt.Println(fileArr)
+	visited := make(map[[2]int]bool)
+	visited[currentPosition] = true
+
+	for {
+		newPosition := move(currentPosition, direction)
+		if !validPosition(newPosition, fileArr) {
+			break
+		}
+
+		if fileArr[newPosition[1]][newPosition[0]] == "#" {
+			direction = (direction + 1) % 4
+		} else {
+			currentPosition = newPosition
+			visited[currentPosition] = true
+		}
+	}
+
+	fmt.Println(len(visited))
 }
 
-func changeDirection(direction int) int {
-	direction = (direction + 1) % 4
-	return direction
+func validPosition(pos [2]int, area [][]string) bool {
+	return pos[1] >= 0 && pos[1] < len(area) && pos[0] >= 0 && pos[0] < len(area[0])
+}
+
+func move(pos [2]int, direction int) [2]int {
+	switch direction {
+	case 0:
+		return [2]int{pos[0], pos[1] - 1}
+	case 1:
+		return [2]int{pos[0] + 1, pos[1]}
+	case 2:
+		return [2]int{pos[0], pos[1] + 1}
+	case 3:
+		return [2]int{pos[0] - 1, pos[1]}
+	}
+	return pos
 }
